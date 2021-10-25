b41ab62997b4c6b78346298d6c4455a9920898c0
MaximeHemon <74669577+MaximeHemon@users.noreply.github.com>
2021-10-25 11:17:00 +0200

    Added Camera movement, Camera Action, PlayerCamera

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,54 @@
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; } = null; 
+
+    public enum GameState
+    {
+        PREPARATION, 
+        RUNNING,
+        END
+    }
+
+    private GameState gameState { get; set; } = GameState.PREPARATION;
+
+    public float _preparationTime = 10;
+
+    private float preparationTime { get { return _preparationTime;  } }
+    private float gameTimer { get; set; } = 0; 
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        switch(gameState)
+        {
+            case GameState.PREPARATION:
+                gameTimer += Time.deltaTime;
+                if(gameTimer >= preparationTime)
+                {
+                    gameTimer = 0;
+                    gameState = GameState.RUNNING;
+                }
+                break;
+
+            case GameState.RUNNING:
+
+                break;
+
+            case GameState.END:
+
+                break; 
+
+        }
+    }
+}
